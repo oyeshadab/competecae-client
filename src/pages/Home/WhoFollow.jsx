95d6ec770721edf2e79e9f1729c7554ce43fbf00
Shadab Hussain <shadabhussain@Shadabs-MacBook-Air.local>
2022-08-24 10:33:54 +0530

    connect with withings

--- src/pages/Home/WhoFollow.jsx
+++ src/pages/Home/WhoFollow.jsx
@@ -3,95 +3,129 @@ import React from "react";
 import { useAuthUser } from "react-auth-kit";
 import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import Button from "../../components/Button/Button";
 
 const WhoFollow = ({ data }) => {
-  const authUser = useAuthUser();
-  const [Follow, setFollow] = useState(null);
-  const [expanded, setExpanded] = useState(false);
-  const [DataForDisplay, setDataForDisplay] = useState([]);
+	const authUser = useAuthUser();
+	const [Follow, setFollow] = useState(null);
+	const [expanded, setExpanded] = useState(false);
+	const [DataForDisplay, setDataForDisplay] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(
-        `${process.env.REACT_APP_API_URL}/follower/user/?id=${
-          authUser().user_id
-        }`
-      )
-      .then((res) => {
-        setFollow(res.data.users);
-        setDataForDisplay(res.data.users.slice(0, 5));
-      })
-      .catch((err) => console.error(err));
-  }, []);
+	useEffect(() => {
+		axios
+			.get(
+				`${process.env.REACT_APP_API_URL}/follower/user/?id=${
+					authUser().user_id
+				}`
+			)
+			.then((res) => {
+				setFollow(res.data.users);
+				setDataForDisplay(res.data.users.slice(0, 5));
+			})
+			.catch((err) => console.error(err));
+	}, []);
 
-  const getFollow = () => {
-    axios
-      .get(
-        `${process.env.REACT_APP_API_URL}/follower/user/?id=${
-          authUser().user_id
-        }`
-      )
-      .then((res) => setFollow(res.data.users))
-      .catch((err) => console.log(err));
-  };
+	const getFollow = () => {
+		axios
+			.get(
+				`${process.env.REACT_APP_API_URL}/follower/user/?id=${
+					authUser().user_id
+				}`
+			)
+			.then((res) => setFollow(res.data.users))
+			.catch((err) => console.log(err));
+	};
 
-  const followPost = (follow) => {
-    axios
-      .post(`${process.env.REACT_APP_API_URL}/follower/follow`, {
-        subscriber_id: follow,
-        user_id: authUser().user_id,
-        followed: true,
-      })
-      .then((res) => {
-        getFollow();
-        toast.success(`Successfully followed ${follow}`);
-      })
-      .catch((err) => console.error(err));
-  };
+	const followPost = (follow) => {
+		axios
+			.post(`${process.env.REACT_APP_API_URL}/follower/follow`, {
+				subscriber_id: follow,
+				user_id: authUser().user_id,
+				followed: true,
+			})
+			.then((res) => {
+				getFollow();
+				toast.success(`Successfully followed ${follow}`);
+			})
+			.catch((err) => console.error(err));
+	};
 
-  return (
-    <div className="bg-yellow-700 rounded-lg p-5">
-      <h4 className="font-poppins text-medium text-28xl mb-3">Who to Follow</h4>
-      <ul className="flex flex-col gap-5">
-        {DataForDisplay
-          ? DataForDisplay.map((post) => {
-              return (
-                <li className="flex items-center gap-3">
-                  <img
-                    className="w-[30px] h-[30px] rounded-full border border-white "
-                    src={post.profile_pic}
-                    alt=""
-                  />
-                  <p className="flex items-center gap-1 flex-grow text-8xl font-montserrat text-gray-500">
-                    <span className="">{post.user_name}</span>
-                    <img src="/images/Frame.svg" alt="" />
-                  </p>
-                  <div>
-                    <button
-                      onClick={() => followPost(post._id)}
-                      type="button"
-                      className="font-medium font-montserrat text-3xl bg-white rounded-md py-2 px-3"
-                    >
-                      Follow
-                    </button>
-                  </div>
-                </li>
-              );
-            })
-          : ""}
-        <button
-          type="button"
-          onClick={() => {
-            setDataForDisplay(expanded ? Follow.slice(0, 5) : Follow);
-            setExpanded(!expanded);
-          }}
-        >
-          {expanded ? "Show Less" : "Show More"}
-        </button>
-      </ul>
-      <ToastContainer />
-    </div>
-  );
+   const ConnectWithWithing = () => {
+      // alert(process.env.REACT_APP_API_URL)
+      // return
+      axios
+			.post(`${process.env.REACT_APP_API_URL}/Withings/connect`, {
+				user_id: authUser().user_id,
+			})
+			.then((res) => {
+				console.log("🚀 ~ file: WhoFollow.jsx ~ line 61 ~ .then ~ res", res)
+				// getFollow();
+				// toast.success(`Successfully followed ${follow}`);
+			})
+			.catch((err) => console.error(err));
+      console.log("🚀 ~ file: WhoFollow.jsx ~ line 66 ~ ConnectWithWithing ~ process.env.REACT_APP_API_URL}/Withings/connect", process.env.REACT_APP_API_URL+'/Withings/connect')
+      alert(authUser().user_id)
+   };
+
+	return (
+		<>
+		<div className="bg-yellow-700 rounded-lg p-5">
+			<h6 className="font-poppins text-small text-22xl mb-3" style={{textAlign: 'center'}}>Connect to Withings</h6>
+			<div>
+				<Button
+					text="Connect"
+					type="secondary"
+					style={{ alignSelf: 'center',width: '100%'}}
+					className="btn-primary"
+					fn={() => {ConnectWithWithing()}}
+				/>
+			</div>
+		</div>
+
+
+		<div className="bg-yellow-700 rounded-lg p-5">
+			<h4 className="font-poppins text-medium text-28xl mb-3">Who to Follow</h4>
+			<ul className="flex flex-col gap-5">
+				{DataForDisplay
+					? DataForDisplay.map((post) => {
+							return (
+								<li className="flex items-center gap-3">
+									<img
+										className="w-[30px] h-[30px] rounded-full border border-white "
+										src={post.profile_pic}
+										alt=""
+									/>
+									<p className="flex items-center gap-1 flex-grow text-8xl font-montserrat text-gray-500">
+										<span className="">{post.user_name}</span>
+										<img src="/images/Frame.svg" alt="" />
+									</p>
+									<div>
+										<button
+											onClick={() => followPost(post._id)}
+											type="button"
+											className="font-medium font-montserrat text-3xl bg-white rounded-md py-2 px-3"
+										>
+											Follow
+										</button>
+									</div>
+								</li>
+							);
+						})
+					: ""}
+				<button
+					type="button"
+					onClick={() => {
+						setDataForDisplay(expanded ? Follow.slice(0, 5) : Follow);
+						setExpanded(!expanded);
+					}}
+				>
+					{expanded ? "Show Less" : "Show More"}
+				</button>
+			</ul>
+			<ToastContainer />
+		</div>
+		</>
+	);
 };
 
 export default WhoFollow;
